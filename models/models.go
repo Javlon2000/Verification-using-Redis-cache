c3774ba3fb8c43f13a9232c7db7d2dcb6e56fe89
Javlon Jurabekov <86648010+Javlon2000@users.noreply.github.com>
2021-11-09 00:19:09 +0500

    Update models.go

--- models/models.go
+++ models/models.go
@@ -1,14 +1,7 @@
 package models
 
-import (
-	"errors"
-	"time"
-  
-    "gorm.io/gorm"
-)
-
 type Check struct {
-	Username string `json:"username" binding:"required"`
+	Username string 
 	Password string 
 }
 
@@ -16,16 +9,4 @@ type InsertDatabase struct {
 	Username string
 	Email string 
 	Password string 
-}
-
-type User struct {
-	Id uint `gorm:"column:user_id"`
-	Email string
-	Username string
-	CreatedAt time.Time
-}
-
-func IsNotFound(row *gorm.DB) bool {
-
-  return errors.Is(row.Error, gorm.ErrRecordNotFound)
 }
